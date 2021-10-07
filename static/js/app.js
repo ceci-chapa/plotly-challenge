5f2f4b7edcef4719f82968eac0a3370774eb04d1
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-10-07 00:43:50 -0500

    adjusting styles

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,7 @@ function generatePlots(patientId) {
       y: selectedPatientSamples.otu_ids.slice(0, 10).map(otu_id => `OTU #${otu_id}`).reverse(),
       text: selectedPatientSamples.otu_labels.slice(0, 10).reverse(),
       marker: {
-        color: 'rgb(194, 83, 83)'
+        color: '#9cdbbf'
       },
       type: 'bar',
       orientation: 'h'
